fef7438ab450326855e7b4765675b66d92105abc
SkiingxMoose <jodesmond@live.com>
2015-12-07 14:32:11 -0500

    minor changes

---
+++ VirologySimulation/src/com/gmail/jdesmond10/virology/main/CampusState.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.gmail.jdesmond10.virology.main;
+
+import sim.engine.SimState;
+
+/**
+ * @author Josh Desmond
+ *
+ */
+public class CampusState extends SimState {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
