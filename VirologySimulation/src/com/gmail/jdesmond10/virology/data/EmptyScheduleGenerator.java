a58cf20cd6d06e5a111cb6e557cba9fb3131eb9b
SkiingxMoose <jodesmond@live.com>
2015-12-10 13:24:57 -0500

    Built schedule generating classes

---
+++ VirologySimulation/src/com/gmail/jdesmond10/virology/data/EmptyScheduleGenerator.java
@@ -0,0 +1,101 @@
+package com.gmail.jdesmond10.virology.data;
+
+import java.util.Map;
+
+import com.gmail.jdesmond10.virology.main.MaxCapicityLecture;
+import com.gmail.jdesmond10.virology.time.MeetingTimes;
+import com.gmail.jdesmond10.virology.time.SimpleDailyMeetingTimes;
+
+import com.google.common.collect.ImmutableMap;
+
+import ec.util.MersenneTwisterFast;
+
+public class EmptyScheduleGenerator {
+
+	private int currentCourseCode = 0;
+	private MersenneTwisterFast random;
+
+	/**
+	 * This is a hardcoded map of (lecture size -> number of lectures). That is,
+	 * the key value pair (75,10) means to have 10 lectures of 75 people be
+	 * generated. Uses Guava's ImmutableMap.
+	 */
+	//@formatter:off
+	private static final Map<Integer, Integer> LECTURE_RULES =
+			ImmutableMap.<Integer, Integer>builder()
+			.put(75, 10)
+			.put(30, 40)
+			.put(10, 10)
+			.build();
+	//@formatter:on
+
+	public EmptyScheduleGenerator(MersenneTwisterFast random) {
+		this.random = random;
+	}
+
+	/**
+	 * This generates a brand new empty LectureScheduler, populated with empty
+	 * classes (but existing classes).
+	 * 
+	 * @see LectureScheduler
+	 * @return LectureScheduler to be populated with Classes.
+	 */
+	public static LectureScheduler generateNewSchedule(
+			MersenneTwisterFast random) {
+		EmptyScheduleGenerator e = new EmptyScheduleGenerator(random);
+		return e.generateSchedule();
+
+	}
+
+	/**
+	 * Builds a schedule from scratch, using the rules defined by LECTURE_RULES.
+	 * 
+	 * @return new LectureScheduler.
+	 */
+	protected LectureScheduler generateSchedule() {
+		LectureScheduler lectureScheduler = new LectureScheduler(random);
+
+		for (Map.Entry<Integer, Integer> entry : LECTURE_RULES.entrySet()) {
+			Integer lectureSize = entry.getKey();
+			Integer numOfLectures = entry.getValue();
+
+			for (int i = numOfLectures; i > 0; i--) {
+
+				lectureScheduler.addClass(new MaxCapicityLecture(
+						getNextCourseTime(), getNextCourseCode(lectureSize),
+						lectureSize));
+			}
+		}
+
+		return lectureScheduler;
+	}
+
+	/**
+	 * Returns a unique course code. Includes the capacity just for making the
+	 * course code include some information about itself.
+	 * 
+	 * @param lectureSize
+	 *            Capacity of course
+	 * @return String to be used for course code.
+	 */
+	private String getNextCourseCode(int lectureSize) {
+		currentCourseCode++;
+		return String.format("C%s-%s", currentCourseCode, lectureSize);
+	}
+
+	/**
+	 * Generates a new meeting time for use when generating the schedule.
+	 * 
+	 * @param lectureSize
+	 * @return
+	 */
+	private MeetingTimes getNextCourseTime() {
+		// TODO Make this random, not evenly distributed.
+		// time represents the starting time of the course. This will rotate
+		// between 8 to 14
+		final int time = currentCourseCode % 7 + 8;
+
+		return new SimpleDailyMeetingTimes(time);
+	}
+
+}
---
+++ VirologySimulation/src/com/gmail/jdesmond10/virology/data/LectureScheduler.java
@@ -0,0 +1,112 @@
+package com.gmail.jdesmond10.virology.data;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.gmail.jdesmond10.virology.main.Lecture;
+import com.gmail.jdesmond10.virology.main.MaxCapicityLecture;
+import com.gmail.jdesmond10.virology.main.Student;
+
+import ec.util.MersenneTwisterFast;
+
+public class LectureScheduler {
+
+	// SUGGESTION this should probably just be a Lecture...
+	private List<MaxCapicityLecture> masterList;
+	private MersenneTwisterFast random;
+
+	public LectureScheduler(MersenneTwisterFast random) {
+		this.masterList = new LinkedList<MaxCapicityLecture>();
+	}
+
+	/**
+	 * Adds a lecture to the given list of lectures.
+	 * 
+	 * @param lecture
+	 *            Lecture to add.
+	 */
+	public void addClass(MaxCapicityLecture lecture) {
+		this.masterList.add(lecture);
+	}
+
+	public int getNumberOfLectures() {
+		return masterList.size();
+	}
+
+	/**
+	 * Randomly assigns a student to three classes.
+	 */
+	public void registerStudentForClasses(Student student) {
+		Collection<Lecture> threeLectures = getRandomThreeLectures();
+
+		for (Iterator<Lecture> iterator = threeLectures.iterator(); iterator
+				.hasNext();) {
+			Lecture lecture = iterator.next();
+
+			student.registerForClass(lecture);
+		}
+	}
+
+	/**
+	 * Ensures that the three lectures are unique, and do not have course timing
+	 * conflicts.
+	 * 
+	 * @return
+	 */
+	private Collection<Lecture> getRandomThreeLectures() {
+		Lecture one;
+		Lecture two = null;
+		Lecture three = null;
+
+		one = getRandomLecture();
+
+		boolean searchingForSecondLecture = true;
+
+		while (searchingForSecondLecture) {
+			// SUGGESTION optimize this by keeping track of what meetings have
+			// already been checked. That is you can say, getRandomLecture, if
+			// it's been retrieved before, try again.
+			two = getRandomLecture();
+
+			if (!two.equals(one) && !two.time.conflictsWith(one.time)) {
+				// If they're different
+				// And if they're not conflicting
+
+				searchingForSecondLecture = false;
+			}
+
+			// SUGGESTION have this short circuit if you keep checking and you
+			// run out of options.
+		}
+
+		boolean searchingForThirdLecture = true;
+
+		while (searchingForThirdLecture) {
+			three = getRandomLecture();
+
+			if (!three.equals(one) && !three.time.conflictsWith(one.time)) {
+				// If they're different
+				// And if they're not conflicting
+
+				if (!three.equals(two) && !three.time.conflictsWith(two.time)) {
+					searchingForThirdLecture = false;
+				}
+			}
+		}
+
+		return Arrays.asList(one, two, three);
+	}
+
+	/**
+	 * @return a random lecture from the list of lectures.
+	 */
+	private Lecture getRandomLecture() {
+		int maxIndex = getNumberOfLectures() - 1;
+		// SUGGESTION Ensure this is actually correct logic. No tests.
+		return masterList.get(random.nextInt() % maxIndex);
+	}
+
+}
